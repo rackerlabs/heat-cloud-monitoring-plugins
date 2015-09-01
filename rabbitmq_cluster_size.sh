be0a56427b7d6cf2239b01c272b1f141e481e46e
Daniel Givens <daniel.givens@rackspace.com>
2015-09-01 13:25:12 -0500

    Added handling of newlines in the rabbitmq cluster_status output

--- rabbitmq_cluster_size.sh
+++ rabbitmq_cluster_size.sh
@@ -4,7 +4,7 @@ output=$( /usr/sbin/rabbitmqctl status 2>&1 )
 
 if [ $? -eq 0 ]
 then
-    cluster_size=$( /usr/sbin/rabbitmqctl cluster_status | grep running_nodes | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/,/ /' | wc -w )
+    cluster_size=$( /usr/sbin/rabbitmqctl cluster_status -q | sed ':a;N;$!'"ba;s/',\n/', /g" | sed 's/[[:space:]]//g' | grep running_nodes | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/,/ /g' | wc -w )
     echo "status Cluster okay"
     echo "metric cluster_size int $cluster_size"
 else
